e1f27506289457a18d4af42e1eac132873fb392a
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-10 03:40:21 +0000

    Wed, Apr 10, 2024, 11:40 AM +08:00

--- mario.c
+++ mario.c
@@ -1,16 +1,34 @@
-get_size()
-print_grid()
-
 #include<cs50.h>
 #include<stdio.h>
+
+void print_grid(int n);
+int get_size();
 
 int main(void)
 {
     int n = get_size();
-    print_grid();
+    print_grid(n);
 }
 
 int get_size()
 {
-    
+    int n;
+    do
+    {
+        n = get_int("Size:");
+    }
+    while (n < 1);
+    return n;
+}
+
+void print_grid(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
