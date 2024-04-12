02fec42516ee0ae51ed2266ab20d627d7fc92ce2
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-12 06:55:24 +0000

    Fri, Apr 12, 2024, 2:55 PM +08:00

--- uppercase.c
+++ uppercase.c
@@ -4,21 +4,21 @@
 
 int main(int argc, string argv[])
 {
-    string s = argv[];
+    string s = argv;
     if(argc < 2)
     {
         printf("Missing the command line argument!");
     }
     else
     {
-        for(int i = 0; i < strlen(argv); i++)
+        for(int i = 0; i < strlen(s); i++)
         {
 
-            if(argv[i] > 'a' && argv[i] < 'z')
+            if(s[i] > 'a' && s[i] < 'z')
             {
-                argv[i] = argv[i] - 32;
+                s[i] = s[i] - 32;
             }
         }
     }
-    printf("%s", argv);
+    printf("%s", s);
 }
