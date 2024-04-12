c0fe28a13bae095aeeb30abccc23353e4be32d24
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-12 10:06:24 +0000

    Fri, Apr 12, 2024, 6:06 PM +08:00

--- recursion.c
+++ recursion.c
@@ -11,9 +11,14 @@ int main(void)
 
 void draw(int n)
 {
-    if(n)
+    for(int i = 0; i < n; i++)
     {
-        
+        printf("#");
     }
+    printf("\n");
     n--;
+    if(!n)
+    {
+        draw(n);
+    }
 }
