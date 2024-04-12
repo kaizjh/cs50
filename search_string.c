c3ee372fb9b05ce37df102b5e52d97dcfbc5238e
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-12 04:30:33 +0000

    Fri, Apr 12, 2024, 12:30 PM +08:00

--- search_string.c
+++ search_string.c
@@ -7,6 +7,8 @@ int main(void)
     string strings[] = {"echo", "flash", "great", "marvel", "amazing", "fxxk"};
     string s = get_string("string:");
     for (int i = 0; i < 6; i++)
+    /**if you do copy-paste, it is really important to look at the loops,
+    to aviod getting the error"Segmention fault(core dumped)", in here, it means i is way too big.*/
     {
         if(!strcmp(strings[i], s))
         {
