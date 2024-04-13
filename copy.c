d32645657ca62f9ab0f1709f242b46a9f45c479c
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-13 12:40:36 +0000

    Sat, Apr 13, 2024, 8:40 PM +08:00

--- copy.c
+++ copy.c
@@ -1,10 +1,11 @@
+#include<cs50.h>
 #include <stdio.h>
 #include<string.h>
 
 int main(void)
 {
     char *s = "hi!";
-    char *t =malloc(strlen(s) + 1);
+    char *t = malloc(strlen(s) + 1);
 
     for(int i = 0; i < strlen(s) + 1; i++)
     {
