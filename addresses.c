5277228ec73fb71851eb52604a11555227d75237
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-13 03:46:34 +0000

    Sat, Apr 13, 2024, 11:46 AM +08:00

--- addresses.c
+++ addresses.c
@@ -17,11 +17,14 @@ int main(void)
 {
     char *s = "Irv";
     printf("%s\n", s);
-    printf("%p\n", &s);
+    printf("%p\n\n", &s);
 
     printf("%p\n", s);
     printf("%p\n", &s[0]);
     printf("%p\n", &s[1]);
     printf("%p\n", &s[2]);
     printf("%p\n", &s[3]);
+    printf("%p\n", &s[4]);
+    printf("%p\n", &s[5]);
+    
 }
