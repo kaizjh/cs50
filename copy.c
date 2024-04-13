33fe44cf799ace89b5b558e73372d6f83f2da3db
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-13 12:59:29 +0000

    Sat, Apr 13, 2024, 8:59 PM +08:00

--- copy.c
+++ copy.c
@@ -8,11 +8,14 @@ int main(void)
     char *s = "hi!";
     char *t = malloc(strlen(s) + 1);
 
-    for(int i = 0; i < strlen(s); i++)
+    for (int i = 0, n =strlen(s) + 1; i < n; i++)
+    /**for(int i = 0; i < strlen(s); i++), here is a little promotion above,
+     * just make sure there is no 'calling function' in the condition (middle of the for loop) ,
+     * because we don't need to call it again again and again (in the middle of the loop).*/
     {
         t[i] = s[i];
     }
 
     t[0] = toupper(t[0]);
-    printf("t:%s\n", t);
+    printf ("t:%s\n", t);
 }
