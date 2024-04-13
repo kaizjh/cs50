cdfcc71e52ffd94bf89cc370851e80f4b4656781
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-13 12:39:43 +0000

    Sat, Apr 13, 2024, 8:39 PM +08:00

---
+++ copy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void)
+{
+    char *s = "hi!";
+    char *t =malloc(strlen(s) + 1);
+
+    for(int i = 0; i < strlen(s) + 1; i++)
+    {
+        t[i] = s[i];
+    }
+
+    printf("t:%s", t);
+}
