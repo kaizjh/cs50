37094f30ec361f9009e50cd7c98f0c1b75c7b0cb
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-14 07:25:03 +0000

    Sun, Apr 14, 2024, 3:25 PM +08:00

---
+++ lecture5_DataStructure/list.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int *list[] = malloc(3*sizeof(int));
+    list[0] = 0;
+    list[1] = 1;
+    list[2] = 2;
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+}
