a5633f4aedb15862efd07296f68030691b835ed7
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-14 07:27:53 +0000

    Sun, Apr 14, 2024, 3:27 PM +08:00

--- L5/list.c
+++ L5/list.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include<stdlib.h>
 
 int main(void)
 {
-    int *list[] = malloc(3*sizeof(int));
+    int *list = malloc(3*sizeof(int));
     list[0] = 0;
     list[1] = 1;
     list[2] = 2;
