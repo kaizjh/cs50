6874d2b7062f1a3a2e9c83d9d3ad19b98d5845e3
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-14 02:18:51 +0000

    Sun, Apr 14, 2024, 10:18 AM +08:00

--- garbage.c
+++ garbage.c
@@ -4,6 +4,9 @@
 int main(void)
 {
     int scores[1024];
+    /**garbage values are indeed a term, which means that a variable you might have defined
+     * that you might have declared but never give it a explicit value (initialized).
+     * then the compile will poke around in the memory, and who knows what there going be?*/
     for (int i = 0; i < 1024; i++)
     {
         printf("%i\n", scores[i]);
