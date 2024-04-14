343b0bc703bf605d3399a63e1ab4df2cf03d023e
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-14 09:43:35 +0000

    Sun, Apr 14, 2024, 5:43 PM +08:00

--- linkedlist.c
+++ linkedlist.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct node //node in here is kind of wild, but it is like a function declaration, it must be here.
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(int argc, char *argv[])
+{
+    node *list = NULL;
+    for (int i = 1; i < argc; i++)
+    {
+        
+    }
+}
