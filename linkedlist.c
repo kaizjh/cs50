1e562a448ba3bc903bb0bec35e5511352c6d6145
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-14 09:49:50 +0000

    Sun, Apr 14, 2024, 5:49 PM +08:00

--- linkedlist.c
+++ linkedlist.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node //node in here is kind of wild, but it is like a function declaration, it must be here.
+typedef struct node
+//node in here is kind of wild, but it is like a function declaration, it must be here.
 {
     int number;
     struct node *next;
@@ -12,7 +13,16 @@ int main(int argc, char *argv[])
 {
     node *list = NULL;
     for (int i = 1; i < argc; i++)
+    //why 'int i = 1' here not 'int i = 0' is that the first value in argv is actually the program's name.
     {
-        
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+        if(n == NULL)
+        {
+            return 1;
+        }
+        n -> number = number;
+        n -> next = NULL;
     }
 }
