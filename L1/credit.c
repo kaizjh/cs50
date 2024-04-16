006a393a9ae01daca04cd4bb69eeb8740007b90d
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-16 13:48:26 +0000

    Tue, Apr 16, 2024, 9:48 PM +08:00

--- L1/credit.c
+++ L1/credit.c
@@ -7,4 +7,31 @@ int main(void)
     string number = get_string("Number:");
 
     printf("%i\n", strlen(number));
+
+    int sum;
+    for (int i = 0; i < 16; i = i + 2)
+    {
+        if (number[i] > 52)
+        {
+            sum = sum + 1 + (number[i] -48) * 2 - 10;
+        }
+        else
+        {
+            sum = sum + (number[i] - 48) * 2;
+        }
+    }
+
+    for (int j = 1; j < 16; j = j + 2)
+    {
+        sum = sum + number[j] - 48;
+    }
+
+    if (sum % 10 == 0)
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
 }
