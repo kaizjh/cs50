47513be3df6d28bd47cf8d4cd23bd5bfb658be71
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-16 12:59:40 +0000

    Tue, Apr 16, 2024, 8:59 PM +08:00

--- L1/credit.c
+++ L1/credit.c
@@ -6,8 +6,10 @@ int main(void)
     string number = get_string("Number:");
     int sum;
 
-    for (int i = 0; i < )
-
+    for (int i = 0; i < 8; i++)
+    {
+        
+    }
 
     printf("%s\n", number);
     printf("%i\n", number[0] -48);
