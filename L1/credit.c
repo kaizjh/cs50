7e806a5c5f5f6d20ff9cdd418942a19000a16bbf
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-16 13:55:16 +0000

    Tue, Apr 16, 2024, 9:55 PM +08:00

--- L1/credit.c
+++ L1/credit.c
@@ -18,4 +18,25 @@ int main(void)
     }
 }
 
-// Calculate checksum
+// Calculate Checksum
+int calculate(string)
+{
+    int sum = 0, length = strlen(number);
+    for (int i = 0; i < length; i = i + 2)
+    {
+        if (number[i] > 52)
+        {
+            sum = sum + 1 + (number[i] -48) * 2 - 10;
+        }
+        else
+        {
+            sum = sum + (number[i] - 48) * 2;
+        }
+    }
+
+    for (int j = 1; j < length; j = j + 2)
+    {
+        sum = sum + number[j] - 48;
+    }
+    return sum;
+}
