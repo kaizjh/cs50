40801dd46be1b675b562a3564e69090496db4ea6
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-16 13:58:20 +0000

    Tue, Apr 16, 2024, 9:58 PM +08:00

--- L1/credit.c
+++ L1/credit.c
@@ -6,7 +6,7 @@ int main(void)
 {
     string number = get_string("Number:");
 
-
+    int sum = calculate(number);
 
     if (sum % 10)
     {
@@ -19,24 +19,27 @@ int main(void)
 }
 
 // Calculate Checksum
-int calculate(string)
+int calculate(string s)
 {
-    int sum = 0, length = strlen(number);
+    int sum = 0, length = strlen(s);
     for (int i = 0; i < length; i = i + 2)
     {
-        if (number[i] > 52)
+        if (s[i] > 52)
         {
-            sum = sum + 1 + (number[i] -48) * 2 - 10;
+            sum = sum + 1 + (s[i] -48) * 2 - 10;
         }
         else
         {
-            sum = sum + (number[i] - 48) * 2;
+            sum = sum + (s[i] - 48) * 2;
         }
     }
 
     for (int j = 1; j < length; j = j + 2)
     {
-        sum = sum + number[j] - 48;
+        sum = sum + s[j] - 48;
     }
     return sum;
 }
+
+//Check for Card Length and Starting Digits
+
