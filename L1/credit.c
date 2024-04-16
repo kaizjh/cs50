41363a689c78f43b7a8cbe6f1086c955b838681a
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-16 12:54:09 +0000

    Tue, Apr 16, 2024, 8:54 PM +08:00

--- L1/credit.c
+++ L1/credit.c
@@ -3,6 +3,12 @@
 
 int main(void)
 {
-    unsigned long int number = get_long("Number:");
-    printf("%lu\n", number);
+    string number = get_string("Number:");
+    printf("%s\n", number);
+    printf("%i\n", number[0] -48);
+    printf("%i\n", number[1] -48);
+    printf("%i\n", number[2]);
+    printf("%i\n", number[3]);
+    printf("%i\n", number[4]);
+    printf("%i\n", number[5]);
 }
