0ba5c3c751990cd2509cd36e1d849a3052bb3090
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-16 10:45:23 +0000

    Tue, Apr 16, 2024, 6:45 PM +08:00

--- L1/cash.c
+++ L1/cash.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void greedy(int n);
+
 int main(void)
 {
     int co;
@@ -15,10 +17,33 @@ int main(void)
 void greedy(int n)
 {
     int counts = 0;
-    for(; n > 24; )
+    if(n > 24)
     {
-        counts = n % 25;
-        n = n - i * n;
+        for(; n > 24; counts++)
+        {
+            n = n - 25;
+        }
     }
-    
+    else if(n > 9)
+    {
+        for(; n > 9; counts++)
+        {
+            n = n - 10;
+        }
+    }
+    else if(n > 5)
+    {
+        for(; n > 4; counts++)
+        {
+            n = n - 5;
+        }
+    }
+    else
+    {
+        for(; n > 0; counts++)
+        {
+            n = n - 1;
+        }
+    }
+    printf("%i\n", counts);
 }
