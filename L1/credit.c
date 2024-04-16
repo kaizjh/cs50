a964c8f86f667e8b2ab98d2f0d031fb7d0e8447e
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-16 13:03:16 +0000

    Tue, Apr 16, 2024, 9:03 PM +08:00

--- L1/credit.c
+++ L1/credit.c
@@ -4,11 +4,15 @@
 int main(void)
 {
     string number = get_string("Number:");
-    int sum;
 
-    for (int i = 0; i < 8; i++)
+    int sum = 0;
+    for (int i = 0; i < 16; i = i + 2)
     {
-        
+        sum = sum + number[i] * 2;
+    }
+    for (int j = 1; j < 16; j = j + 2)
+    {
+        sum = sum + number[j];
     }
 
     printf("%s\n", number);
