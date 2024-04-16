739b7ab98f913f586e508626940dc992a73f8b02
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-16 13:53:10 +0000

    Tue, Apr 16, 2024, 9:53 PM +08:00

--- L1/credit.c
+++ L1/credit.c
@@ -6,23 +6,7 @@ int main(void)
 {
     string number = get_string("Number:");
 
-    int sum = 0, length = strlen(number);
-    for (int i = 0; i < length; i = i + 2)
-    {
-        if (number[i] > 52)
-        {
-            sum = sum + 1 + (number[i] -48) * 2 - 10;
-        }
-        else
-        {
-            sum = sum + (number[i] - 48) * 2;
-        }
-    }
 
-    for (int j = 1; j < length; j = j + 2)
-    {
-        sum = sum + number[j] - 48;
-    }
 
     if (sum % 10)
     {
@@ -33,3 +17,5 @@ int main(void)
 
     }
 }
+
+// Calculate checksum
