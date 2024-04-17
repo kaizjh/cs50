3b4df8c9ce00349f88d3fcb397d6e3b81ba57f50
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-17 07:28:56 +0000

    Wed, Apr 17, 2024, 3:28 PM +08:00

--- L2/ps2/scrabble.c
+++ L2/ps2/scrabble.c
@@ -3,16 +3,32 @@
 #include <stdio.h>
 #include <string.h>
 
-string get_word_and_toupper(int number);
+string get_word(int number);
+int score(string p);
 
 int main(void)
 {
-    string p1 = get_word_and_toupper(1);
-    string p2 = get_word_and_toupper(2);
+    string p1 = get_word(1);
+    string p2 = get_word(2);
 
+    int s1 = score(p1);
+    int s2 = score(p2);
+    if(s1 > s2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if(s1 < s2)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
 
 }
 
+//Prompt for the user for words and tuopper them
 string get_word(int number)
 {
     int mark;
@@ -35,4 +51,42 @@ string get_word(int number)
     while (mark);
 
     return p;
+}
+
+//Compute the score of the words.
+int score(string p)
+{
+    int score = 0;
+    for (int i = 0, n = strlen(p); i < n; i++)
+    {
+        if ( p[i] == 'D' || p[i] == 'G')
+        {
+            score = score + 2;
+        }
+        else if (p[i] == 'B' || p[i] == 'C' || p[i] == 'M' || p[i] == 'P')
+        {
+            score = score + 3;
+        }
+        else if ( p[i] == 'F' || p[i] == 'H' || p[i] == 'V' || p[i] == 'W' || p[i] == 'Y')
+        {
+            score = score + 4;
+        }
+        else if( p[i] == 'A')
+        {
+            score = score + 5;
+        }
+        else if( p[i] == 'J' p[i] == 'X')
+        {
+            score = score + 8;
+        }
+        else if( p[i] == 'Q' p[i] == 'Z')
+        {
+            score = score + 10;
+        }
+        else
+        {
+            score++;
+        }
+    }
+    return score;
 }
