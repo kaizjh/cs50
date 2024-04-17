7d02cb77bbd25669df3e8593282afe24b56e497f
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-17 06:29:16 +0000

    Wed, Apr 17, 2024, 2:29 PM +08:00

--- L2/ps2/scrabble.c
+++ L2/ps2/scrabble.c
@@ -6,7 +6,10 @@ int main(void)
     do
     {
         string p1 = get_string("Player 1:");
-        
+        for (int i = 0, n = strlen(p1); i < n; i++)
+        {
+            if(p1[i] > )
+        }
     }
     string p2 = get_string("Player 2:");
 }
