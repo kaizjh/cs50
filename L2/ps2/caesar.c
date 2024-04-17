33ce7b2c28234c1396f83598f9f374400d5a320c
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-17 13:15:26 +0000

    Wed, Apr 17, 2024, 9:15 PM +08:00

--- L2/ps2/caesar.c
+++ L2/ps2/caesar.c
@@ -9,7 +9,11 @@
 // print the ciphertext
 int main(int argc, string argv[])
 {
-    if(argc == 2)
+    if(argc != 2)
+    {
+        printf("Please enter one argument!");
+        return 1;
+    }
     printf("%s\n", argv);
 }
 
