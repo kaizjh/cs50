a9f3cb97f097f8079f0e7bd0fd8abd68e6af85ad
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-17 09:55:32 +0000

    Wed, Apr 17, 2024, 5:55 PM +08:00

--- L2/ps2/readability.c
+++ L2/ps2/readability.c
@@ -1,14 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int index(string s);
+
 int main(void)
 {
     // Prompt the user for some text
     int text = get_string("Text: ");
-    /**Count the L, the average number of letters per 100 words in the text
-     * And the S, the average number of sentences per 100 words in the text
-     * Then compute the Coleman-Liau index
-     */
-    int index = count()
+
+    int index = count(text);
     // Print the Grade X
+}
+
+/**Count the L, the average number of letters per 100 words in the text
+ * And the S, the average number of sentences per 100 words in the text
+ * Then compute the Coleman-Liau index
+ */
+int index(string s)
+{
+    int L = 0, n = strlen(s);
+    for(int i = 0; i < strlen(s); i++)
+    {
+        if ((p[i] < 65 && p[i] > 90) || (p[i] < 122 && p[i] > 97))
+        {
+            letter++;
+        }
+    }
 }
