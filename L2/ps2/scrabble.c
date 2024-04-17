3ee859b9af9fcdd56cb13384864ec7070f341f4d
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-17 07:33:15 +0000

    Wed, Apr 17, 2024, 3:33 PM +08:00

--- L2/ps2/scrabble.c
+++ L2/ps2/scrabble.c
@@ -31,25 +31,16 @@ int main(void)
 //Prompt for the user for words and tuopper them
 string get_word(int number)
 {
-    int mark;
-    string p;
-    do
-    {
-        mark = 0;
-        p = get_string("Player %i:", number);
-        for (int i = 0, n = strlen(p); i < n; i++)
+    string p = get_string("Player %i:", number);
+    for (int i = 0, n = strlen(p); i < n; i++)
         {
             if(p[i] < 65 || p[i] > 122 || (p[i] > 90 && p[i] < 97))
             {
-                printf("Don't enter anything but alphabets.\n");
+                p[i] = 0;
                 mark = 1;
-                break;
             }
             p[i] = toupper(p[i]);
         }
-    }
-    while (mark);
-
     return p;
 }
 
