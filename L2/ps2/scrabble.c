7a201888e93df89cf0e9961ae0fa8b1058b4d231
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-17 06:33:08 +0000

    Wed, Apr 17, 2024, 2:33 PM +08:00

--- L2/ps2/scrabble.c
+++ L2/ps2/scrabble.c
@@ -8,7 +8,10 @@ int main(void)
         string p1 = get_string("Player 1:");
         for (int i = 0, n = strlen(p1); i < n; i++)
         {
-            if(p1[i] > )
+            if(p1[i] < 65 || p1[i] > 122 || (p1[i] > 90 && p1[i] < 97))
+            {
+                printf("Please enter alphabets.")
+            }
         }
     }
     string p2 = get_string("Player 2:");
