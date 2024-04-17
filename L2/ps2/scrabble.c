2b486216bfeda3f5173583b5318f264a4605da4a
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-17 06:35:53 +0000

    Wed, Apr 17, 2024, 2:35 PM +08:00

--- L2/ps2/scrabble.c
+++ L2/ps2/scrabble.c
@@ -1,18 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
+    int mark;
     do
     {
+        mark = 0;
         string p1 = get_string("Player 1:");
         for (int i = 0, n = strlen(p1); i < n; i++)
         {
             if(p1[i] < 65 || p1[i] > 122 || (p1[i] > 90 && p1[i] < 97))
             {
-                printf("Please enter alphabets.")
+                printf("Please enter alphabets.");
+                mark = 1;
+                break;
             }
         }
     }
+    while (mark);
     string p2 = get_string("Player 2:");
 }
