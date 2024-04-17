e029d002123415018678def7e1255dade7f49cc3
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-17 06:45:07 +0000

    Wed, Apr 17, 2024, 2:45 PM +08:00

--- L2/ps2/scrabble.c
+++ L2/ps2/scrabble.c
@@ -9,13 +9,13 @@ int main(void)
     string p2 = get_string("Player 2:");
 }
 
-string get_word(int p)
+string get_word(int number)
 {
     int mark;
     do
     {
         mark = 0;
-        string p = get_string("Player %i:", p);
+        string p = get_string("Player %i:", number);
         for (int i = 0, n = strlen(p); i < n; i++)
         {
             if(p[i] < 65 || p[i] > 122 || (p[i] > 90 && p[i] < 97))
