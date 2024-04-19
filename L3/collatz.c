388164aa58dc7876425ae55504678d116a12ad20
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-19 09:42:29 +0000

    Fri, Apr 19, 2024, 5:42 PM +08:00

---
+++ L3/collatz.c
@@ -0,0 +1,22 @@
+#include <cs50.c>
+#include <stdio.h>
+
+int main()
+{
+    c
+}
+
+int collatz(int n)
+{
+    int step = 1;
+    if (n = 1)
+    {
+        return step;
+    }
+    else if (n % 2)
+    {
+        n = 3 * n + 1;
+        step++;
+    }
+    else
+}
