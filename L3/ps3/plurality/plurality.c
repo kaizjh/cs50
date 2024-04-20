eb2a2c51fb609c57753049392b66205c09bca488
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-20 13:48:38 +0000

    Sat, Apr 20, 2024, 9:48 PM +08:00

--- L3/ps3/plurality/plurality.c
+++ L3/ps3/plurality/plurality.c
@@ -80,7 +80,6 @@ bool vote(string name)
 void print_winner(void)
 {
     candidate winner;
-    winner.name = NULL;
     winner.votes = 0;
     for (int i = 0; i < candidate_count; i++)
     {
@@ -88,19 +87,6 @@ void print_winner(void)
         {
             winner.votes = candidates[i].votes;
             winner.name = candidates[i].name;
-        }
-    }
-    
-
-    if (winner.name)
-    {
-        printf("%s\n", winner.name);
-    }
-    else
-    {
-        for (int i = 0; i < candidate_count; i++)
-        {
-            printf("%s\n", candidates[i].name);
         }
     }
 
