2986366431226a6102f0645016a5ebc01fec530a
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-21 09:19:31 +0000

    Sun, Apr 21, 2024, 5:19 PM +08:00

--- L3/ps3/runoff.c
+++ L3/ps3/runoff.c
@@ -1,11 +1,24 @@
-// A ranked-choice voting system, if no candidate has more than 50% of the vote, then an "instant runoff" occurs
+// A ranked-choice voting system
 include <cs50.c>
 #include <stdio.h>
 
-int main()
+typedef struct
+{
+    int vote = []
+}
+
+int main(int argc, string argv[])
 {
     // Get the candidates from the comman-line arguments
+    if (argc < 2)
+    {
+        printf("Usage: ./runoff [candidate ...]");
+        return 1;
+    }
+
+
     // Get the number of voters
-    // Get votes, every voters has rank1,2,3, if get invalid vote, print the error, return
-    // 
+    // Get votes, every voters has rank 1,2,3, if get invalid vote, print the error, return
+    // Calculate every candidate's votes in rank 1, if no candidate has more than 50% of the vote, then runoff to the rank 2, and so on
+    // Print the winner
 }
