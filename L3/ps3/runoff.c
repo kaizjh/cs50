72b6171069b3fe1f6446515b9c36af7ce46b41d2
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-21 09:06:35 +0000

    Sun, Apr 21, 2024, 5:06 PM +08:00

--- L3/ps3/runoff.c
+++ L3/ps3/runoff.c
@@ -1,7 +1,8 @@
-#include <cs50.c>
+// A ranked-choice voting system, if no candidate has more than 50% of the vote, then an "instant runoff" occurs
+include <cs50.c>
 #include <stdio.h>
 
 int main()
 {
-    
+    // Get the candidates
 }
