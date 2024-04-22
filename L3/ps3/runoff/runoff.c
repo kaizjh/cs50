5b8ed645b331bffa0e6d5be62b3ef461f46ba0ac
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-22 13:28:18 +0000

    Mon, Apr 22, 2024, 9:28 PM +08:00

--- L3/ps3/runoff/runoff.c
+++ L3/ps3/runoff/runoff.c
@@ -146,9 +146,13 @@ void tabulate(void)
         for (int rank = 0; rank < candidate_count; rank++)
         {
             int p = preferences[voter][rank];
-            if (!candidates[p].eliminated)
+            if (candidates[p].eliminated)
             {
-                
+                rank++;
+            }
+            else
+            {
+                candidates[p].votes++;
             }
         }
     }
