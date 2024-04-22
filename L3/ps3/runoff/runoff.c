675b65018a581e04e62f3a4500115b4f40c9a4d8
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-22 13:46:33 +0000

    Mon, Apr 22, 2024, 9:46 PM +08:00

--- L3/ps3/runoff/runoff.c
+++ L3/ps3/runoff/runoff.c
@@ -176,7 +176,7 @@ bool print_winner(void)
     int max_num = 0;
     for (int j = 0; j < candidate_count; j++)
     {
-        if (max == candidates[j].votes)
+        if (max > candidates[j].votes)
         {
             max_num++;
         }
