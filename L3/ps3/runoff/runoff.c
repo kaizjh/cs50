65d09a6632e256ab3e873a2732f5958ed9a4b0cf
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-22 08:49:09 +0000

    Mon, Apr 22, 2024, 4:49 PM +08:00

--- L3/ps3/runoff/runoff.c
+++ L3/ps3/runoff/runoff.c
@@ -140,7 +140,10 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidate[i].eliminated)
+    }
     return;
 }
 
