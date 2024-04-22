97551f097a9152bbdbbda4860ac8a8011cc6be7f
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-22 09:04:25 +0000

    Mon, Apr 22, 2024, 5:04 PM +08:00

--- L3/ps3/runoff/runoff.c
+++ L3/ps3/runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(name, candidate[i].name) == 0)
         {
-            preferences[voter][rank]++;
+            preferences[voter][rank] = i;
             return true;
         }
     }
