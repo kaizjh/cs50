6574ba4a38d4b52b1bb9404a511ae3478140920a
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-22 09:41:54 +0000

    Mon, Apr 22, 2024, 5:41 PM +08:00

--- L3/ps3/runoff/runoff.c
+++ L3/ps3/runoff/runoff.c
@@ -206,7 +206,11 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidate[i].eliminated)
+        {
+            
     return false;
 }
 
