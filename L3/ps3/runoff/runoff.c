f4d473bd65641b0f173ba073e6ed15d6eb4061d3
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-22 13:58:47 +0000

    Mon, Apr 22, 2024, 9:58 PM +08:00

--- L3/ps3/runoff/runoff.c
+++ L3/ps3/runoff/runoff.c
@@ -182,7 +182,7 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int min = 0;
+    int min = 1;
     for (int i = 0; i < candidate_count; i++)
     {
         if (!candidates[i].eliminated)
