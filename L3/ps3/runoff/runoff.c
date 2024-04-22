266e545fce99f6feda239f7e806e9a227b17c0ec
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-22 08:43:25 +0000

    Mon, Apr 22, 2024, 4:43 PM +08:00

--- L3/ps3/runoff/runoff.c
+++ L3/ps3/runoff/runoff.c
@@ -126,7 +126,11 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidate[i].name) == 0)
+            return true;
+    }
     return false;
 }
 
