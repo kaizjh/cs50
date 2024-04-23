044db5cc2520ef6b41ad18d8e9c3ac18a0c57764
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-23 13:20:45 +0000

    Tue, Apr 23, 2024, 9:20 PM +08:00

--- L3/ps3/tideman/tideman.c
+++ L3/ps3/tideman/tideman.c
@@ -191,7 +191,7 @@ void sort_pairs(void)
 }
 
 // A function in lock_pairs by Irving, to judge the graph is cycle or not
-bool is_cycle(int current);
+bool is_cycle(int current, int seen[])
 
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
@@ -203,16 +203,21 @@ void lock_pairs(void)
     }
     for (int i = 0; i < pair_count; i++)
     {
-        if (!is_cycle(i), seen[])
+        if (!is_cycle(i, seen[]))
         {
             locked[pairs[i].winner][pairs[i].loser] = true;
+            seen[i] = i;
         }
     }
     return;
 }
 
-bool is_cycle(int current)
+bool is_cycle(int current, int seen[])
 {
+    if (seen[0] == -1)
+    {
+        return false;
+    }
     int begin_winner = pairs[current].winner;
     while (true)
     {
