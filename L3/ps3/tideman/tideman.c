548d60e8512b889d1139b08c2184aad3e5a320c4
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-23 05:44:25 +0000

    Tue, Apr 23, 2024, 1:44 PM +08:00

--- L3/ps3/tideman/tideman.c
+++ L3/ps3/tideman/tideman.c
@@ -112,7 +112,7 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    for (int rank = 0; rank < candidate_count; rank++)
+    for (int rank = 0; rank < candidate_count - 1; rank++)
     {
         for (int i = 0; i < candidate_count; i++)
             {
