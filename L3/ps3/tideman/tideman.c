2180405887fccab139987a49a03385eb7d03f511
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-23 05:04:37 +0000

    Tue, Apr 23, 2024, 1:04 PM +08:00

--- L3/ps3/tideman/tideman.c
+++ L3/ps3/tideman/tideman.c
@@ -98,7 +98,10 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    for (int i = 0; i < )
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     return false;
 }
 
