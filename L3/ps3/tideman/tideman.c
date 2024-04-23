75c3955680c3e646f816509dd4e99169a1099486
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-23 06:09:06 +0000

    Tue, Apr 23, 2024, 2:09 PM +08:00

--- L3/ps3/tideman/tideman.c
+++ L3/ps3/tideman/tideman.c
@@ -148,7 +148,13 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            preferences[i][j]++;
+        }
+    }
     return;
 }
 
