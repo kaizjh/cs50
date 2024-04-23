c072a120b8a545476b52709b7021e28bb28678a3
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-23 05:28:36 +0000

    Tue, Apr 23, 2024, 1:28 PM +08:00

--- L3/ps3/tideman/tideman.c
+++ L3/ps3/tideman/tideman.c
@@ -114,6 +114,7 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
+        if (ranks[0] = )
         for (int j = 1;  < candidate_count; j++)
         {
             preferences[i][j]++;
