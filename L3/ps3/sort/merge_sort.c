82fb2d80bfa32ccd3f9b966eaf998da70f92cbcc
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-24 10:27:06 +0000

    Wed, Apr 24, 2024, 6:27 PM +08:00

--- L3/ps3/sort/merge_sort.c
+++ L3/ps3/sort/merge_sort.c
@@ -9,11 +9,14 @@ int main(void)
 
 merge_sort(int start, int final, int arr[])
 {
-    if (start <= final)
+    if (start >= final)
     {
         return;
     }
 
     int mid = (final + start) / 2;
-    
+    merge_sort(start, mid);
+    merge_sort(mid, final);
+
+
 }
