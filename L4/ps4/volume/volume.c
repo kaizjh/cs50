28d64bc79c9b25e16804c4765027e3870853d2ae
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-25 06:54:14 +0000

    Thu, Apr 25, 2024, 2:54 PM +08:00

--- L4/ps4/volume/volume.c
+++ L4/ps4/volume/volume.c
@@ -33,12 +33,8 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    char ch;
-    for (int i = 0; i < HEADER_SIZE; i++)
-    {
-        ch = fgetc(input);
-        fputc(ch, output);
-    }
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
 
     // TODO: Read samples from input file and write updated data to output file
     for (int i = HEADER_SIZE; ch != EOF; i++)
