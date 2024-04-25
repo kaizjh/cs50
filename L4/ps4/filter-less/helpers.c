d024865426e19b7a54bbe1c9fcdd14ca79953d39
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-25 13:57:19 +0000

    Thu, Apr 25, 2024, 9:57 PM +08:00

--- L4/ps4/filter-less/helpers.c
+++ L4/ps4/filter-less/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed < )
+            if (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed < 0x888888)
         }
     }
     return;
