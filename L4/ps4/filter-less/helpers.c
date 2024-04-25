9ddf18d97b2e09542ed5453b79cde91f45603968
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-25 14:50:22 +0000

    Thu, Apr 25, 2024, 10:50 PM +08:00

--- L4/ps4/filter-less/helpers.c
+++ L4/ps4/filter-less/helpers.c
@@ -21,6 +21,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 
