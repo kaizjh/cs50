5560060cc5cd364e3c67a14088b10a19273af7ba
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-26 10:36:51 +0000

    Fri, Apr 26, 2024, 6:36 PM +08:00

--- L4/ps4/recover/recover.c
+++ L4/ps4/recover/recover.c
@@ -9,14 +9,14 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    FILE *input = fopen(argc[1],"r");
-    if (input = NULL)
+    FILE *input = fopen(argv[1],"r");
+    if (input == NULL)
     {
         printf("Can not open the input file!");
         return 1;
     }
     FILE *output = fopen("1.jpeg","w");
-    if (input = NULL)
+    if (input == NULL)
     {
         printf("Can not open the output file!");
         return 1;
@@ -25,10 +25,10 @@ int main(int argc, char *argv[])
     uint8_t buffer[512];
     while(fread(buffer, sizeof(uint8_t), 512, input) == 512)
     {
-        fread(buffer, sizeof(uint8_t), 512, input);
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] >= 0xe0 && buffer[3] <= 0xef)
         {
             fwrite(buffer, sizeof(uint8_t), 512, output);
+            break;
         }
 
     }
