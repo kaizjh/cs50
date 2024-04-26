275870a012bd917dbf12b23d66766a8e0d225fe5
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-26 02:15:46 +0000

    Fri, Apr 26, 2024, 10:15 AM +08:00

--- L4/ps4/filter-less/helpers.c
+++ L4/ps4/filter-less/helpers.c
@@ -25,15 +25,21 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue = 0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue;
+
             image[i][j].rgbtGreen = 0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue;
             image[i][j].rgbtRed = 0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue;
             if (image[i][j].rgbtRed > 255)
                 image[i][j].rgbtRed = 255;
             if (image[i][j].rgbtGreen > 255)
                 image[i][j].rgbtGreen = 255;
-            if (image[i][j].rgbtBlue > 255)
+            if (0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue > 255)
+            {
                 image[i][j].rgbtBlue = 255;
+            }
+            else
+            {
+                image[i][j].rgbtBlue = 0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue;
+            }
         }
     }
     return;
