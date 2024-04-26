664e7d16465a83bff23794c4da53fab628037f39
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-26 03:03:25 +0000

    Fri, Apr 26, 2024, 11:03 AM +08:00

--- L4/ps4/filter-less/helpers.c
+++ L4/ps4/filter-less/helpers.c
@@ -92,7 +92,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j];
+            for (int )
         }
     }
     return;
