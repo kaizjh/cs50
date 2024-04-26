0a0bc02dd67ecfce54387af6967da3f150b0f955
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-26 02:35:22 +0000

    Fri, Apr 26, 2024, 10:35 AM +08:00

--- L4/ps4/filter-less/helpers.c
+++ L4/ps4/filter-less/helpers.c
@@ -63,7 +63,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     int mid = width / 2;
-    RGBTRIPLE tmp;
+    int tmp;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
