d37a8dc3daccb24e04303617402873ebee899332
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-28 06:51:30 +0000

    Sun, Apr 28, 2024, 2:51 PM +08:00

--- L5/list.c
+++ L5/list.c
@@ -10,6 +10,21 @@ node;
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        printf("Usage: ./list numbers ...\n");
+    }
+
     node *list = NULL;
-    
+
+    for (int i = 1; i < argc; i++)
+    {
+        int num = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+        n -> number = num;
+        n -> next = list;
+
+        list = n;
+    }
 }
