1e4cdc2d7944cf82d1c7524d811d4af5574b6c2d
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-28 08:20:59 +0000

    Sun, Apr 28, 2024, 4:20 PM +08:00

--- L5/list.c
+++ L5/list.c
@@ -27,4 +27,11 @@ int main(int argc, char *argv[])
 
         list = n;
     }
+
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n", ptr -> number);
+        ptr = ptr -> next;
+    }
 }
