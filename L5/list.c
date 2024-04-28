e6d96732463bb2df5431796ce8999f57627d03bb
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-28 06:34:38 +0000

    Sun, Apr 28, 2024, 2:34 PM +08:00

--- L5/list.c
+++ L5/list.c
@@ -10,29 +10,5 @@ node;
 
 int main(void)
 {
-    int *list = malloc(3*sizeof(int));
-    if (list == NULL)
-    {
-        return 1;
-    }
     
-    list[0] = 0;
-    list[1] = 1;
-    list[2] = 2;
-
-    int *tmp = realloc(list, 4*sizeof(int));
-    if(tmp == NULL)
-    {
-        free(list);
-        return 1;
-    }
-    list = tmp;
-    list[3] = 3;
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%i\n", list[i]);
-    }
-    free(list);
-    return 0;
 }
