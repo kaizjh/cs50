7d1bb42c304cac1ebe36c1887c1c4e648a8648d0
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-29 12:49:35 +0000

    Mon, Apr 29, 2024, 8:49 PM +08:00

--- L5/section/list.c
+++ L5/section/list.c
@@ -54,9 +54,9 @@ int main(void)
 bool unload(node *list)
 {
     // TODO: Free all allocated nodes
+    node *ptr = NULL;
     for (int i = 0; i < LIST_SIZE; i++)
     {
-        node *ptr = NULL;
         ptr = list->next;
         free(list);
         list = ptr;
