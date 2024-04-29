2f501037d4c5c138b1ecc3ead7342ff36e4005bb
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-29 09:37:13 +0000

    Mon, Apr 29, 2024, 5:37 PM +08:00

--- L5/ps5/inheritance/inheritance.c
+++ L5/ps5/inheritance/inheritance.c
@@ -53,7 +53,12 @@ person *create_family(int generations)
         new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person->alleles[0] =
+        char r = random_allele();
+        if (r == new_person->parents[0]->alleles[0] || r == new_person->parents[0]->alleles[1])
+        {
+            new_person->alleles[0] = new_person->parents[0]->alleles[0];
+        }
+
 
     }
 
