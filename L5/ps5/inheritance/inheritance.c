4df78fafc0f947b86f90179fa5bfdea0ad8e28cf
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-04-29 09:27:52 +0000

    Mon, Apr 29, 2024, 5:27 PM +08:00

--- L5/ps5/inheritance/inheritance.c
+++ L5/ps5/inheritance/inheritance.c
@@ -49,9 +49,11 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        
+        parent0 = new_person;
+        parent1 =
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        
     }
 
     // If there are no generations left to create
