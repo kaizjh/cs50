58a7576fccced82dfe4711f88f13cb4fdc671954
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 10:08:45 +0000

    Thu, May 2, 2024, 6:08 PM +08:00

--- L6/scores.py
+++ L6/scores.py
@@ -3,3 +3,12 @@ scores = [72, 73, 33]
 
 average = sum(scores) / len(scores)
 print(f"Average:{average}")
+
+
+scores = []
+for i in range(3):
+    score = int(input("Score:"))
+    scores.append(score)
+    
+average = sum(scores) / len(scores)
+print(f"Average:{average}")
