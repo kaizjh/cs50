3a994e781890dd076b7c20fc0fd22aa46248ecf3
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 08:51:11 +0000

    Thu, May 2, 2024, 4:51 PM +08:00

--- L6/mario.py
+++ L6/mario.py
@@ -1,10 +1,19 @@
-# There is no "do ... while...(in c)" loop in python, but there is a way to do the same thing
-# That is using a infinite loop:
-while True:
-    n = int(input("Height: "))
-    if n > 0:
-        break
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
 
-for i in range(n):
-    print("#")
 
+def get_height():
+    # There is no "do ... while...(in c)" loop in python, but there is a way to do the same thing
+    # That is using a infinite loop:
+    while True:
+        n = int(input("Height: "))
+        if n > 0:
+            break
+    # Break the while loop then return n, that means the scope problem in c does not exist in python
+    # And if we return in the while loop is also right
+    return n
+
+
+main()
