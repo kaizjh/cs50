0a9cfb3fa0ec83a5ad34efb67e837688f025bc50
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 13:08:36 +0000

    Thu, May 2, 2024, 9:08 PM +08:00

--- greet.py
+++ greet.py
@@ -3,3 +3,7 @@ from sys import argv
 # Using slices of lists, I can skip the first argument
 for arg in argv[1:]:
     print(arg)
+
+# Or just print the second argument
+for arg in argv[1:2]:
+    print("@",arg)
