a0cd20404b869021cf6c25241e2f65a2cac429b5
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 14:14:52 +0000

    Thu, May 2, 2024, 10:14 PM +08:00

---
+++ L6/phonebook.py
@@ -0,0 +1,3 @@
+# This is similar to scores.py "scores = []"
+# This actually the shorthand version of this: people = dict(), which means create a empty dictionary
+people = {}
--- L6/scores.py
+++ L6/scores.py
@@ -4,11 +4,12 @@ scores = [72, 73, 33]
 average = sum(scores) / len(scores)
 print(f"Average:{average}")
 
-
+# This actually the shorthand version of this: scores = list(), which means create a empty list
 scores = []
+
 for i in range(3):
     score = int(input("Score:"))
     scores.append(score)
-    
+
 average = sum(scores) / len(scores)
 print(f"Average:{average}")
