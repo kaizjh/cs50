729702bdfbd3b1bfbe0951ded24adca7f87d61c6
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 09:51:56 +0000

    Thu, May 2, 2024, 5:51 PM +08:00

--- L6/mario1.py
+++ L6/mario1.py
@@ -1,1 +1,7 @@
+# Get more details from https://docs.python.org/3/library/functions.html#print
+print("#" * 4)
+
+# These 3 lines below, doing the same thing as 1 line above, and this is the powerful of python
+for i in range(4):
+    print("#", end="")
 print()
