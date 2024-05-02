7d0b1954fe79741c8f5ceac4764c4af13ecfeb89
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 09:56:10 +0000

    Thu, May 2, 2024, 5:56 PM +08:00

--- L6/mario1.py
+++ L6/mario1.py
@@ -1,7 +1,24 @@
-# Get more details from https://docs.python.org/3/library/functions.html#print
-print("#" * 4)
+# Get more details about print() from https://docs.python.org/3/library/functions.html#print
+print("#" * 3)
+
 
 # These 3 lines below, doing the same thing as 1 line above, and this is the powerful of python
-for i in range(4):
+for i in range(3):
     print("#", end="")
 print()
+
+
+print()
+
+
+for i in range(3):
+    print("#" * 3)
+
+
+print()
+
+
+for i in range(3):
+    for j in range(3):
+        print("#", end='')
+    print()
