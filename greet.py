833e4e715d3bfb71133e1157b26dbb5f8a521adb
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 14:09:26 +0000

    Thu, May 2, 2024, 10:09 PM +08:00

--- greet.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-# Using slices of lists, I can skip the first argument, and "1: " means: 1 to the end
-for arg in argv[1:]:
-    print(arg)
-
-# Or just print the second argument
-for arg in argv[1:2]:
-    print("@",arg)
