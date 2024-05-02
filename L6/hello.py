b45b4fb16d71891db102a0f610c7d5bd4c119048
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 04:12:49 +0000

    Thu, May 2, 2024, 12:12 PM +08:00

--- L6/hello.py
+++ L6/hello.py
@@ -1,8 +1,9 @@
-from CS50 import get_string
+from cs50 import get_string
 
 print("hello, world")
 
-answer = get_string("What's your name?")
+answer = get_string("What's your name? ")
 print("hello, answer")
 print("hello, {answer}")
 print(f"hello, {answer}")
+print("hello", answer)
