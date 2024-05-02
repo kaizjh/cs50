2de1e2b873177e1e535fc6171c1a713744c51399
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 13:50:15 +0000

    Thu, May 2, 2024, 9:50 PM +08:00

--- exit.py
+++ exit.py
@@ -1,6 +1,7 @@
 import sys
 
-# If we "from sys import argv" then we can just use argv rather than sys.argv
+# If we "from sys import argv" then we can just use "argv" rather than "sys.argv"
+# And "sys.argv" always tells us where the function it comes from, so I think this style is good for me --Irving
 if len(sys.argv) != 2:
     print("Missing command-line argument")
     sys.exit(1)
