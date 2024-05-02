9a034e05e27ed98340090bb6ac202e4ffb795ec8
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 13:54:59 +0000

    Thu, May 2, 2024, 9:54 PM +08:00

---
+++ names.py
@@ -0,0 +1,12 @@
+import sys
+
+names = ["Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"]
+
+name = input("Name: ")
+
+if name in names:
+    print("Founded")
+    sys.exit(0)
+
+print("Not found")
+sys.exit(1)
