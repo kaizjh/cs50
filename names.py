6f909b3d7bacca20294683dac93542fdc8dcc086
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 14:00:36 +0000

    Thu, May 2, 2024, 10:00 PM +08:00

--- names.py
+++ names.py
@@ -8,11 +8,11 @@ if name in names:
     print("Founded")
     sys.exit(0)
 
+# The for loop below is doing the same thing as the if above
 for n in names:
     if name == n:
         print("Founded")
         sys.exit(0)
-
 
 print("Not found")
 sys.exit(1)
