f3d91e22c2b81eaf6107bacc66671b9129416376
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-02 08:09:45 +0000

    Thu, May 2, 2024, 4:09 PM +08:00

--- L6/meow.py
+++ L6/meow.py
@@ -1,4 +1,6 @@
-
+# It turns out that there is no need to code main function in every source file in python
+# And if you do code a main function in python, don't forget to call it
+# And usually we call the main() in the bottom of the file, so that we can use other functions defined after main(), but used by main()
 
 def main():
     for i in range(3):
