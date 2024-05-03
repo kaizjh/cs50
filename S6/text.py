9c8ef1db8669d9bf897d35ea27904320944a4e5f
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-03 09:46:24 +0000

    Fri, May 3, 2024, 5:46 PM +08:00

--- S6/text.py
+++ S6/text.py
@@ -1,5 +1,10 @@
 text = "In the great green room"
 
-for word in text:
-    for c in word:
-        print(c)
+for word in text.split():
+    print(word)
+
+print("-----------------------------")
+
+for word in text.split()[2:]:
+    print(word)
+
