c8c8a7da0de0a5a56fbfc1ce8fac31782a4a1728
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-03 05:47:33 +0000

    Fri, May 3, 2024, 1:47 PM +08:00

--- ps6/mario.py
+++ ps6/mario.py
@@ -5,10 +5,9 @@ while True:
     if height <= 8 and height >= 1:
         break
 
-for i in [0height:
+for i in range(height):
     print(" " * (height - i), end="")
     print("#" * i, end="  ")
-
-for i in height:
     print("#" * i, end="")
     print(" " * (height - i))
+
