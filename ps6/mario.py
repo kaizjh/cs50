6f7f6e2cc83140d3e7dfc8bf4f8db2f117338708
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-03 06:16:50 +0000

    Fri, May 3, 2024, 2:16 PM +08:00

--- ps6/mario.py
+++ ps6/mario.py
@@ -4,12 +4,7 @@ while True:
         height = input("Height: ")
     except ValueError:
         print("Not an integer")
-        continue
 
-    if height <= 8 and height >= 1:
-        break
-    else:
-        print("Enter a positive integer between 1 and 8")
 
 for i in range(height):
     print(" " * (height - i -1), end="")
