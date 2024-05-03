e8ea7c6b59315176990b6a892ecda800e693c2c9
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-03 14:10:02 +0000

    Fri, May 3, 2024, 10:10 PM +08:00

--- S6/books.py
+++ S6/books.py
@@ -1,10 +1,11 @@
 books = []
 
-for i in range(3):
-    book = dict
+for i in range(2):
+    book = dict()
     book["author"] = input("Enter a author: ")
-    book["title"] = input('Enter a title: ')
+    book["title"] = input("Enter a title: ")
     books.append(book)
+
 
 for book in books:
     print(f"{book['author']} wrote {book['title']}")
