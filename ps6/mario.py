ba5246399035621c182f136f84ce07fb68a75d70
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-03 06:20:13 +0000

    Fri, May 3, 2024, 2:20 PM +08:00

--- ps6/mario.py
+++ ps6/mario.py
@@ -1,7 +1,11 @@
 # Print a pyramid like mario
 while True:
     try:
-        height = input("Height: ")
+        height = int(input("Height: "))
+        if height <= 8 and height >= 1:
+            break
+        else:
+            print("Enter a positive integer between 1 and 8")
     except ValueError:
         print("Not an integer")
 
