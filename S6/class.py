1c176fffeb2a4cf0d00277caa59ab9c6738310d0
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-03 03:26:07 +0000

    Fri, May 3, 2024, 11:26 AM +08:00

--- S6/class.py
+++ S6/class.py
@@ -1,1 +1,18 @@
-class Student:
+class Student():
+
+    def __init__(self, name, id):
+        self.name = name
+        self.id  = id
+
+    def changeID(self, id):
+        self.id = id
+
+    def print(self):
+        print("{} - {}".format(self.name, self.id))
+
+
+jane = Student("Jane", 19)
+jane.print()
+
+jane.changeID(11)
+jane.print()
