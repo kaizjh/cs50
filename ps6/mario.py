09e2aa68423b0cdb0315b7702b7dd3f4b929e0e8
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-03 06:25:38 +0000

    Fri, May 3, 2024, 2:25 PM +08:00

--- ps6/mario.py
+++ ps6/mario.py
@@ -14,4 +14,4 @@ for i in range(height):
     print(" " * (height - i -1), end="")
     print("#" * (i + 1), end="  ")
     print("#" * (i + 1), end="")
-    print(" " * (height - i - 1), end="!")
+    print(" " * (height - i - 1))
