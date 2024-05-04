a268890df852e985ff8a95bed7651dc58a5d422c
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 14:26:54 +0000

    Sat, May 4, 2024, 10:26 PM +08:00

--- ps6/dna/dna.py
+++ ps6/dna/dna.py
@@ -13,6 +13,8 @@ def main():
     with open (sys.argv[1]) as file:
         reader = csv.DictReader(file)
         print(reader.fieldnames)
+        for row in reader:
+            print(row)
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
