cad3bbd32a7f24b157d4148f9f04982b0f01fdaf
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 02:51:48 +0000

    Sat, May 4, 2024, 10:51 AM +08:00

--- ps6/credit.py
+++ ps6/credit.py
@@ -1,8 +1,11 @@
 import cs50
+import re
 
 # Get the number and make sure it's entirely numeric
 number = cs50.get_int("Number: ")
 print(number)
 
-# Check the number's length, using regular expression opreation
+
+# Use regular expression opreation to check number's length and its
+def check_the_credit
 string = str(number)
