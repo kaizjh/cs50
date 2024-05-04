cb527f9ce7fa70ad48cc99df5b961e51e5c689de
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 03:01:19 +0000

    Sat, May 4, 2024, 11:01 AM +08:00

--- ps6/credit.py
+++ ps6/credit.py
@@ -5,11 +5,18 @@ def main:
     # Get the number and make sure it's entirely numeric
     number = cs50.get_int("Number: ")
     print(number)
-    
+    valid_credit
+
 
 
 # Use regular expression opreation to check number's length
-def valid_credit:
-    string = str(number)
-    pattern = r'^\d{' + str
+def valid_credit(number, length):
+    # Define the regular expression pattern
+    pattern = r'^\d{' + str(length) + r'}$'
+
+    # Check if input matches the pattern
+    if re.match(pattern, str(number)):
+        return True
+    else:
+        return False
 
