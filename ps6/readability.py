9d36af71adc1846e41726db04ce61ff24180ce8a
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 08:45:44 +0000

    Sat, May 4, 2024, 4:45 PM +08:00

--- ps6/readability.py
+++ ps6/readability.py
@@ -1,30 +1,24 @@
-def main():
-    text = input("Text: ")
+text = input("Text: ")
 
-    texts = text.lower()
-    words = 1
-    letters = 0
-    sentences = 0
-    for c in texts:
-        print(c)
-        # Count the words
-        if c == ' ':
-            words += 1
-        # Count the letters
-        if c >='a' and c <= 'z':
-            letters += 1
+texts = text.lower()
+words = 1
+letters = 0
+sentences = 0
+for c in texts:
+    print(c)
+    # Count the words
+    if c == ' ':
+        words += 1
+    # Count the letters
+    if c >='a' and c <= 'z':
+        letters += 1
 
-        # Count the sentences
-        if c == '!' or c == '?' or c == '.':
-            sentences += 1
-    print(words, letters, sentences)
+    # Count the sentences
+    if c == '!' or c == '?' or c == '.':
+        sentences += 1
 
-    L = letters / words * 100
-    print(L)
-    S = sentences / words * 100
-    print(S)
-    index = 0.0588 * L - 0.296 * S - 15.8
-    
+L = letters / words * 100
+S = sentences / words * 100
+index = 0.0588 * L - 0.296 * S - 15.8
+print(index)
 
-
-main()
