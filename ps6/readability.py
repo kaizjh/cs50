13c43b52717d617b0ce9d442f181ff70c43e1441
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 08:36:46 +0000

    Sat, May 4, 2024, 4:36 PM +08:00

--- ps6/readability.py
+++ ps6/readability.py
@@ -1,5 +1,4 @@
 def main():
-    text = []
     text = input("Text: ")
 
     texts = text.lower()
@@ -7,7 +6,9 @@ def main():
 
     words = 1
     letters = 0
+    sentences = 0
     for c in texts:
+        print(c)
         # Count the words
         if c == ' ':
             words += 1
@@ -16,7 +17,10 @@ def main():
             letters += 1
 
         # Count the sentences
-        
+        if c == '!' or c == '?' or c == '.':
+            sentences += 1
+
+    print(words, letters, sentences)
 
 
 main()
