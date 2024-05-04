3dbbc9e3504e2bf38df3052470799367e717c028
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 02:48:50 +0000

    Sat, May 4, 2024, 10:48 AM +08:00

--- ps6/credit.py
+++ ps6/credit.py
@@ -1,4 +1,8 @@
 import cs50
 
+# Get the number and make sure it's entirely numeric
 number = cs50.get_int("Number: ")
 print(number)
+
+# Check the number's length, using regular expression opreation
+string = str(number)
