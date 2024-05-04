b56e0635dfb1078180b9de78d55f22d9791d8600
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 07:48:52 +0000

    Sat, May 4, 2024, 3:48 PM +08:00

--- ps6/credit.py
+++ ps6/credit.py
@@ -7,21 +7,15 @@ def main():
     # Convert number from int to str for checking
     str_number = str(number)
 
-    if is_desired_length(str_number, 13) and is_VISA(str_number):
+    if is_desired_length(str_number, 13) or is_desired_length(str_number, 16) and is_VISA(str_number):
         print("VISA")
-        return 0
     elif is_desired_length(str_number, 15) and is_AMEX(str_number):
         print("AMEX")
-        return 0
-    elif is_desired_length(str_number, 16):
-        if is_VISA(str_number):
-            print("VISA")
-            return 0
-        elif is_MASTERCARD(str_number):
-            print("MASTERCARD")
-            return 0
-
-    print("INVALID")
+    elif is_desired_length(str_number, 16) and is_MASTERCARD(str_number):
+        print("MASTERCARD")
+    else:
+        print("INVALID")
+        
     return 0
 
 
