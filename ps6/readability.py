29db2c5dd117d2f8bf9ac6f120db4b97e7c3a785
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 08:26:26 +0000

    Sat, May 4, 2024, 4:26 PM +08:00

--- ps6/readability.py
+++ ps6/readability.py
@@ -1,11 +1,15 @@
 def main():
     text = []
     text = input("Text: ")
+    text.lower()
+    print(text)
 
     words = 1
     for c in text:
-        
+        # Count the words
         if c == ' ':
             words += 1
+        # Count the letters
+        
 
 main()
