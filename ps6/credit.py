f5f1c228567f60e5fd5af37ec4785b641e7d6075
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 03:21:17 +0000

    Sat, May 4, 2024, 11:21 AM +08:00

--- ps6/credit.py
+++ ps6/credit.py
@@ -5,16 +5,36 @@ def main():
     # Get the number and make sure it's entirely numeric
     number = cs50.get_int("Number: ")
     print(number)
-    
-    if valid_credit(number, 16):
-        print(16)
 
+    if is_desired_length(number, 15):
+        if is_AEMX:
+            ...
+        else:
+            print("INVALID\n")
+            return
+    elif is_desired_length(number, 13):
+        if is_VISA:
+            ...
+        else:
+            print("INVALID\n")
+            return
+    elif is_desired_length(number, 16):
+        if is_VISA:
+            ...
+        elif is_MASTERCARD:
+            ...
+        else:
+            print("INVALID\n")
+            return
+    else:
+        print("INVALID\n")
+        return
 
 
 # Use regular expression opreation to check number's length
-def valid_credit(number, length):
+def is_desired_length(number, desired_length):
     # Define the regular expression pattern
-    pattern = r'^\d{' + str(length) + r'}$'
+    pattern = r'^\d{' + str(desired_length) + r'}$'
 
     # Check if input matches the pattern
     if re.match(pattern, str(number)):
