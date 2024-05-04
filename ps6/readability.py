cd5c813d0e88d45ab5a2a4c9f4fe8674a8557ef3
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 08:33:26 +0000

    Sat, May 4, 2024, 4:33 PM +08:00

--- ps6/readability.py
+++ ps6/readability.py
@@ -1,16 +1,22 @@
 def main():
     text = []
     text = input("Text: ")
-    
+
     texts = text.lower()
     print(texts)
 
     words = 1
+    letters = 0
     for c in texts:
         # Count the words
         if c == ' ':
             words += 1
         # Count the letters
+        if c >='a' and c <= 'z':
+            letters += 1
+
+        # Count the sentences
+        
 
 
 main()
