bdb9d24e5059de54cecb5c9addfc04a29e8a21ea
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-04 14:39:30 +0000

    Sat, May 4, 2024, 10:39 PM +08:00

--- ps6/dna/dna.py
+++ ps6/dna/dna.py
@@ -18,7 +18,7 @@ def main():
         rows = []
         for row in reader:
             rows.append(row)
-        print(rows)
+        print(rows[1].value[1])
 
     # TODO: Read DNA sequence file into a variable
     with open (sys.argv[2]) as file:
