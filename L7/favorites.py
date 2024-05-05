422984dcfbfddac4adc60b3d824c88f9f1d52081
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-05 04:40:06 +0000

    Sun, May 5, 2024, 12:40 PM +08:00

---
+++ L7/favorites.py
@@ -0,0 +1,17 @@
+import csv
+
+with open("favorites.csv", "r") as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        favorite = row[1]
+        print(favorite, end='')
+
+print("----------------------------")
+
+with open("favorites.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        favorite = row["language"]
+        print(favorite, end='')
