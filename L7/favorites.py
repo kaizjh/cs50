240d5437d531927d968fa048d8f1cc3a5d060688
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-05 06:37:09 +0000

    Sun, May 5, 2024, 2:37 PM +08:00

--- L7/favorites.py
+++ L7/favorites.py
@@ -1,5 +1,6 @@
 import csv
 
+#Use csv.reader returns a list[]
 with open("favorites.csv", "r") as file:
     reader = csv.reader(file)
 
@@ -7,11 +8,26 @@ with open("favorites.csv", "r") as file:
         favorite = row[1]
         print(favorite, end='  ')
 
-print("----------------------------")
 
+print("----------------------------")
+# Use csv.DictReader returns a dict{}
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
         favorite = row["language"]
         print(favorite, end='  ')
+
+
+print("---------------------------------")
+# Down here is a more dynamic version, and count the favorites
+with open("favorites.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    counts = {}
+    for row in reader:
+        if favorite in counts:
+            counts[favorite] += 1
+        else:
+            # Whenever we see a new language, we create a lang
+            counts[favorite] = 1
