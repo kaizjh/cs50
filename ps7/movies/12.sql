81d94a1398a7fb0b9581f2fd1e788658d2c801b0
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-07 09:44:10 +0000

    Tue, May 7, 2024, 5:44 PM +08:00

--- ps7/movies/12.sql
+++ ps7/movies/12.sql
@@ -1,6 +1,7 @@
-SELECT title
+SELECT movies.title
 FROM movies
 JOIN stars, people
 ON movies.id = stars.movie_id
 AND stars.person_id = people.id
-WHERE 
+WHERE people.name = 'Bradley Cooper'
+AND people.name = 'Jennifer Lawrence';
