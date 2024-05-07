c1a2360a71884931500277a93a3f4daa8e65bdff
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-07 02:29:26 +0000

    Tue, May 7, 2024, 10:29 AM +08:00

--- L7/favorites1.py
+++ L7/favorites1.py
@@ -9,6 +9,14 @@ rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?", favor
 # The code below would do the same thing as a Python way, but you will expose yourself to potential hack or attacks by trusting the user's input, so don't do this
 # rows = db.execute(f"SELECT COUNT(*) AS n FROM favorites WHERE problem = '{favorite}'")
 
+# For example, if there is a code for account log in:
+# rows = db.execute(f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'")
+# and when I typed in the username literally as:    malan@harvard.edu'--    , and no password, and the execute code will become:
+# rows = db.execute(f"SELECT * FROM users WHERE username = 'malan@harvard.edu'--' AND password = '{password}'")
+# and because -- means there is a comment comes in the right, then it becomes:
+# rows = db.execute(f"SELECT * FROM users WHERE username = 'malan@harvard.edu'")
+# that means I skip the password, and heck in the malan@harvard.edu's accout, without his password, that is horrible
+
 for row in rows:
     print(row['n'])
 
