0f03b2774f2aa384f247e532ac20d062e0b539a2
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-07 10:12:40 +0000

    Tue, May 7, 2024, 6:12 PM +08:00

--- ps7/movies/13.sql
+++ ps7/movies/13.sql
@@ -1,42 +1,4 @@
-sqlite> .schema
-CREATE TABLE directors (
-    movie_id INTEGER NOT NULL,
-    person_id INTEGER NOT NULL,
-    FOREIGN KEY(movie_id) REFERENCES movies(id),
-    FOREIGN KEY(person_id) REFERENCES people(id)
-);
-CREATE TABLE movies (
-    id INTEGER,
-    title TEXT NOT NULL,
-    year NUMERIC,
-    PRIMARY KEY(id)
-);
-CREATE TABLE people (
-    id INTEGER,
-    name TEXT NOT NULL,
-    birth NUMERIC,
-    PRIMARY KEY(id)
-);
-CREATE TABLE ratings (
-    movie_id INTEGER NOT NULL,
-    rating REAL NOT NULL,
-    votes INTEGER NOT NULL,
-    FOREIGN KEY(movie_id) REFERENCES movies(id)
-);
-CREATE TABLE stars (
-    movie_id INTEGER NOT NULL,
-    person_id INTEGER NOT NULL,
-    FOREIGN KEY(movie_id) REFERENCES movies(id),
-    FOREIGN KEY(person_id) REFERENCES people(id)
-);
-
-
-
-
-
-
-
-SELECT name FROM people
+SELECT DISTINCT name FROM people
 WHERE id IN (
     SELECT person_id FROM stars
     WHERE movie_id IN (
@@ -48,10 +10,9 @@ WHERE id IN (
             ON movies.id = stars.movie_id
             AND stars.person_id = people.id
             WHERE people.name ='Kevin Bacon'
-            AND people.birth = 1958;
+            AND people.birth = 1958
 
         )
     )
 )
-
-
+AND people.name != 'Kevin Bacon';
