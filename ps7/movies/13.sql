d4d69475f02c90a49a1e82a60015c7219c0900ac
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-07 10:10:39 +0000

    Tue, May 7, 2024, 6:10 PM +08:00

--- ps7/movies/13.sql
+++ ps7/movies/13.sql
@@ -40,6 +40,18 @@ SELECT name FROM people
 WHERE id IN (
     SELECT person_id FROM stars
     WHERE movie_id IN (
-        
+        SELECT id FROM movies
+        WHERE title IN (
+            SELECT movies.title
+            FROM movies
+            JOIN stars, people
+            ON movies.id = stars.movie_id
+            AND stars.person_id = people.id
+            WHERE people.name ='Kevin Bacon'
+            AND people.birth = 1958;
+
+        )
     )
 )
+
+
