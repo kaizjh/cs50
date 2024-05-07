7f7eea69502b90eeaa9734ce3c70fbbdc9c6530d
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-07 09:13:45 +0000

    Tue, May 7, 2024, 5:13 PM +08:00

--- ps7/movies/8.sql
+++ ps7/movies/8.sql
@@ -1,5 +1,6 @@
 SELECT people.name
 FROM people
 JOIN stars, movies
-ON people.id = stars.person_id, stars.movie_id = movies.id
+ON people.id = stars.person_id
+AND stars.movie_id = movies.id
 WHERE movies.title = 'Toy Story';
