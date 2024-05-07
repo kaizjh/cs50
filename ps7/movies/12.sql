1a2bc4e53bf07fa41221c00961d379f5702ea637
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-07 10:00:56 +0000

    Tue, May 7, 2024, 6:00 PM +08:00

--- ps7/movies/12.sql
+++ ps7/movies/12.sql
@@ -5,7 +5,10 @@ WHERE id IN (
         SELECT id FROM people
         WHERE name = 'Bradley Cooper'
     )
-    AND person_id IN (
+)
+AND id IN (
+    SELECT movie_id FROM stars
+    WHERE person_id IN (
         SELECT id FROM people
         WHERE name = 'Jennifer Lawrence'
     )
