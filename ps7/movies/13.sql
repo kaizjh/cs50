10790578ec09bb501eb3de100dada0c01368c9b6
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-07 10:04:26 +0000

    Tue, May 7, 2024, 6:04 PM +08:00

--- ps7/movies/13.sql
+++ ps7/movies/13.sql
@@ -29,3 +29,17 @@ CREATE TABLE stars (
     FOREIGN KEY(movie_id) REFERENCES movies(id),
     FOREIGN KEY(person_id) REFERENCES people(id)
 );
+
+
+
+
+
+
+
+SELECT name FROM people
+WHERE id IN (
+    SELECT person_id FROM stars
+    WHERE movie_id IN (
+        
+    )
+)
