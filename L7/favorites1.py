5511f34de7d94ab6f801031b2aa9bb8f70d2b280
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-07 01:47:03 +0000

    Tue, May 7, 2024, 9:47 AM +08:00

---
+++ L7/favorites1.py
@@ -0,0 +1,9 @@
+import cs50
+
+db = cs50.SQL("sqlite:///favorites.db")
+
+favorite = cs50.get_string("Favorites: ")
+
+rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = 'Mario'")
+
+print(rows[n])
