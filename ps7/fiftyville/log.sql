e56b5c5f2a1a9589c3f20c2a56fae5f92ad344ee
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 10:07:29 +0000

    Wed, May 8, 2024, 6:07 PM +08:00

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -1,5 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Try to find more infomation from crime_scene_reports since
+-- "All you know is that the theft took place on July 28, 2023 and that it took place on Humphrey Street."
 SELECT  id, description
 FROM    crime_scene_reports
 WHERE   street = 'Humphrey Street'
@@ -11,3 +13,16 @@ AND     day = 28;
      Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
 297, Littering took place at 16:36. No known witnesses.
 
+-- Try to find more infomation from bakery_security_logs since the first clue we got from crime_scene_reports:
+-- "Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery."
+SELECT  *
+FROM    bakery_security_logs
+WHERE   year = 2023
+AND     month = 7
+AND     day = 28
+AND     hour = 10
+AND     minute = 15;
+
+-- Nothing returns, bad
+
+-- Try to find more infomation from bakery_security_logs since the first clue we got from crime_scene_reports:
--- ps7/fiftyville/tmp1.sql
+++
@@ -1,5 +0,0 @@
-SELECT  *
-FROM    bakery_security_logs
-WHERE   year = 2023
-AND     month = 7
-AND     day = 28;
