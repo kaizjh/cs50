f0192c16cbef9e22c61f968a3e4981862128bc43
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 15:00:16 +0000

    Wed, May 8, 2024, 11:00 PM +08:00

--- ps7/fiftyville/tmp1.sql
+++ ps7/fiftyville/tmp1.sql
@@ -2,6 +2,7 @@
         In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
         The thief then asked the person on the other end of the phone to purchase the flight ticket. |
 
+-- 
 SELECT *
 FROM        flights
 WHERE       year = 2023
@@ -9,3 +10,9 @@ AND         month = 7
 AND         day = 29
 ORDER BY    hour, minute ASC
 LIMIT 1;
+
++----+-------------------+------------------------+------+-------+-----+------+--------+
+| id | origin_airport_id | destination_airport_id | year | month | day | hour | minute |
++----+-------------------+------------------------+------+-------+-----+------+--------+
+| 36 | 8                 | 4                      | 2023 | 7     | 29  | 8    | 20     |
++----+-------------------+------------------------+------+-------+-----+------+--------+
