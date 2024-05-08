2f2e661e82029c20969a463171f2f0c72c40da00
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 15:08:08 +0000

    Wed, May 8, 2024, 11:08 PM +08:00

--- ps7/fiftyville/tmp1.sql
+++ ps7/fiftyville/tmp1.sql
@@ -3,7 +3,7 @@
         The thief then asked the person on the other end of the phone to purchase the flight ticket. |
 
 -- Check the transcript 163 for earliest flight on 2023-7-29
-SELECT *
+SELECT      *
 FROM        flights
 WHERE       year = 2023
 AND         month = 7
@@ -16,3 +16,15 @@ LIMIT 1;
 +----+-------------------+------------------------+------+-------+-----+------+--------+
 | 36 | 8                 | 4                      | 2023 | 7     | 29  | 8    | 20     |
 +----+-------------------+------------------------+------+-------+-----+------+--------+
+
+    -- Then looking for the airports of this flight
+    SELECT *
+    FORM airports
+    WHERE id IN (
+        SELECT      origin_airport_id
+        FROM        flights
+        WHERE       year = 2023
+        AND         month = 7
+        AND         day = 29
+        
+    )
