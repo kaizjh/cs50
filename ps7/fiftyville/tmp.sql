4c0b4f1b9ddf47a4d0248e1730650aba8e98249d
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 09:49:44 +0000

    Wed, May 8, 2024, 5:49 PM +08:00

--- ps7/fiftyville/tmp.sql
+++ ps7/fiftyville/tmp.sql
@@ -1,6 +1,4 @@
--- Keep a log of any SQL queries you execute as you solve the mystery.
-
-SELECT  id, description
+SELECT  description
 FROM    crime_scene_reports
 WHERE   street = 'Humphrey Street'
 AND     year = 2023
