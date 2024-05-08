bf3e0596e1f383ed68314e1fe7e74a76f98ff5ff
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 10:16:27 +0000

    Wed, May 8, 2024, 6:16 PM +08:00

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -9,9 +9,9 @@ AND     year = 2023
 AND     month = 7
 AND     day = 28;
 
-295, Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
-     Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
-297, Littering took place at 16:36. No known witnesses.
+| 295 | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
+        Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
+| 297 | Littering took place at 16:36. No known witnesses.
 
 -- Try to find more infomation from bakery_security_logs since the first clue we got from crime_scene_reports:
 -- "Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery."
@@ -25,4 +25,15 @@ AND     minute = 15;
 
 -- Nothing returns, bad
 
--- Try to find more infomation from bakery_security_logs since the first clue we got from crime_scene_reports:
+-- Try to find more infomation from interviews since the second clue we got from crime_scene_reports:
+-- "Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery."
+SELECT  id, transcript
+FROM    interviews
+WHERE   year = 2023
+AND     month = 7
+AND     day = 28
+AND     transcript like '%bakery%';
+
+| 161 | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
+| 162 | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
+| 163 | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
