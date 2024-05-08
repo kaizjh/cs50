1d29ad2a93fd6d9f61497adf0e5100733725ef03
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 14:06:53 +0000

    Wed, May 8, 2024, 10:06 PM +08:00

--- ps7/fiftyville/tmp1.sql
+++ ps7/fiftyville/tmp1.sql
@@ -3,4 +3,6 @@
 in the bakery parking lot and drive away. If you have security footage from the bakery parking lot,
 you might want to look for cars that left the parking lot in that time frame.                                                          |
 
-SELECT 
+SELECT *
+FROM bakery_security_logs
+WHERE 
