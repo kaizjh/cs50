b21e8b3a33110a1fe6e02b8bcc7f05c0270559a5
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 14:56:28 +0000

    Wed, May 8, 2024, 10:56 PM +08:00

--- ps7/fiftyville/tmp1.sql
+++ ps7/fiftyville/tmp1.sql
@@ -1,3 +1,10 @@
 | 163 | As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
         In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
         The thief then asked the person on the other end of the phone to purchase the flight ticket. |
+
+SELECT *
+FROM flights
+WHERE   year = 2023
+AND     month = 7
+AND     day = 29
+ORDER BY 
