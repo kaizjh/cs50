c1756426e18b3462b5dcd0e196ceca876bd6a3c4
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 14:12:30 +0000

    Wed, May 8, 2024, 10:12 PM +08:00

--- ps7/fiftyville/tmp1.sql
+++ ps7/fiftyville/tmp1.sql
@@ -1,13 +1,8 @@
-
-| 161 | Sometime within ten minutes of the theft, I saw the thief get into a car
-in the bakery parking lot and drive away. If you have security footage from the bakery parking lot,
-you might want to look for cars that left the parking lot in that time frame.                                                          |
-
 SELECT *
 FROM bakery_security_logs
 WHERE   year = 2023
 AND     month = 7
 AND     day = 28
 AND     hour = 10
-AND     minute < 25
-AND     minute > 5
+AND     minute <= 25
+AND     minute >= 5;
