acd186ebde16a5ac3afd0b43d55763e627663976
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 09:45:37 +0000

    Wed, May 8, 2024, 5:45 PM +08:00

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -1,2 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+SELECT  *
+FROM    crime_scene_reports
+WHERE   street = 'Humphrey Street'
+AND     year = 2023
+AND     month = 7
+AND     day = 28;
