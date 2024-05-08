b6c75fb8b33177ebd0d18fe535bf289f7f85e1a5
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-08 14:28:01 +0000

    Wed, May 8, 2024, 10:28 PM +08:00

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -12,7 +12,7 @@ AND     day = 28;
 | 295 | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
         Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
 
-        -- Check the descriptino 295, 1 "Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery."
+        -- Select the descriptino 295, line 1, looking for more information
         SELECT  *
         FROM    bakery_security_logs
         WHERE   year = 2023
@@ -22,7 +22,7 @@ AND     day = 28;
         AND     minute = 15;
         -- Nothing returns
 
-        -- Check the description 295, 2 "Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery."
+        -- Check the description 295, line 2,
         SELECT  id, transcript
         FROM    interviews
         WHERE   year = 2023
