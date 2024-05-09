3419f49b97706a0cd432722d56806a31c8166a9b
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-09 01:25:13 +0000

    Thu, May 9, 2024, 9:25 AM +08:00

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -133,29 +133,29 @@ AND     day = 28;
                         SELECT  *
                         FROM    people
                         WHERE   license_plate IN(
-                        SELECT  license_plate
-                        FROM    bakery_security_logs
-                        WHERE   year = 2023
-                        AND     month = 7
-                        AND     day = 28
-                        AND     hour = 10
-                        AND     minute <= 25
-                        AND     minute >= 5
-                        AND     activity = 'exit'
-
-                        AND     passport_number IN(
-                                SELECT  passport_number
-                                FROM    passengers
-                                WHERE   flight_id IN(
-                                SELECT  id
-                                FROM    flights
+                                SELECT  license_plate
+                                FROM    bakery_security_logs
                                 WHERE   year = 2023
                                 AND     month = 7
-                                AND     day = 29
-                                AND     hour = 8
-                                AND     minute = 20
-                        )
-                        )
+                                AND     day = 28
+                                AND     hour = 10
+                                AND     minute <= 25
+                                AND     minute >= 5
+                                AND     activity = 'exit'
+
+                                AND     passport_number IN(
+                                        SELECT  passport_number
+                                        FROM    passengers
+                                        WHERE   flight_id IN(
+                                        SELECT  id
+                                        FROM    flights
+                                        WHERE   year = 2023
+                                        AND     month = 7
+                                        AND     day = 29
+                                        AND     hour = 8
+                                        AND     minute = 20
+                                        )
+                                )
                         );
 
                         -- Here are the suspects in a smaller scope
