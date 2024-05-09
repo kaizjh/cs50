2aae8d3a3069b88bc59e9d1c303f21c7839298c4
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-09 01:32:28 +0000

    Thu, May 9, 2024, 9:32 AM +08:00

--- ps7/fiftyville/tmp1.sql
+++ ps7/fiftyville/tmp1.sql
@@ -1,4 +1,14 @@
-SELECT 
+SELECT  person_id
+FROM    bank_accounts
+WHERE   account_number IN(
+    SELECT  account_number
+    FROM    atm_transactions
+    WHERE   atm_location = 'Leggett Street'
+    AND     year = 2023
+    AND     month = 7
+    AND     day = 28
+    AND     transaction_type = 'withdraw';
+)
 
 
 SELECT  id
