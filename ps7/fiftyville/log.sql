de7d1def2a64fa6f59a1b66e1761de723b2e0280
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-09 02:33:55 +0000

    Thu, May 9, 2024, 10:33 AM +08:00

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -258,7 +258,7 @@ AND     day = 28;
                         | 4  | LGA          | LaGuardia Airport | New York City |
                         +----+--------------+-------------------+---------------+
 
-                -- According to the transcript 163 line 1, and the main suspects from line 133's check, I am going to check this two suspects' phone_call information on theft day
+                -- According to the transcript 163 line 1, and the main suspects from line 133's check, I am going to check these two suspects' phone_call information on theft day
 
                 -- Check the first suspect, but returns nothing
                 SELECT  *
