e9acb4af3f27fd86feaeaf837180786e6bd65ff9
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-09 01:30:21 +0000

    Thu, May 9, 2024, 9:30 AM +08:00

--- ps7/fiftyville/tmp1.sql
+++ ps7/fiftyville/tmp1.sql
@@ -1,27 +1,30 @@
-SELECT  *
-                        FROM    people
-                        WHERE   license_plate IN(
-                                SELECT  license_plate
-                                FROM    bakery_security_logs
-                                WHERE   year = 2023
-                                AND     month = 7
-                                AND     day = 28
-                                AND     hour = 10
-                                AND     minute <= 25
-                                AND     minute >= 5
-                                AND     activity = 'exit'
+SELECT 
 
-                                AND     passport_number IN(
-                                        SELECT  passport_number
-                                        FROM    passengers
-                                        WHERE   flight_id IN(
-                                        SELECT  id
-                                        FROM    flights
-                                        WHERE   year = 2023
-                                        AND     month = 7
-                                        AND     day = 29
-                                        AND     hour = 8
-                                        AND     minute = 20
-                                        )
-                                )
-                        );
+
+SELECT  id
+FROM    people
+WHERE   license_plate IN(
+        SELECT  license_plate
+        FROM    bakery_security_logs
+        WHERE   year = 2023
+        AND     month = 7
+        AND     day = 28
+        AND     hour = 10
+        AND     minute <= 25
+        AND     minute >= 5
+        AND     activity = 'exit'
+
+        AND     passport_number IN(
+                SELECT  passport_number
+                FROM    passengers
+                WHERE   flight_id IN(
+                SELECT  id
+                FROM    flights
+                WHERE   year = 2023
+                AND     month = 7
+                AND     day = 29
+                AND     hour = 8
+                AND     minute = 20
+                )
+        )
+);
