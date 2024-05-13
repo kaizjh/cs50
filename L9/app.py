018f834c02cda79a33129575287209d77bbe9c0e
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-13 01:42:43 +0000

    Mon, May 13, 2024, 9:42 AM +08:00

--- L9/app.py
+++ L9/app.py
@@ -7,4 +7,7 @@ app = Flask(__name__)
 # This is known as a decorator in Python, not a flask feature
 @app.route("/")
 def index():
-    return index.html
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
