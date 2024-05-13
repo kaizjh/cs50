748092cddeffa01dd4a9ee5bc3975f6c16de8f3f
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-13 12:22:35 +0000

    Mon, May 13, 2024, 8:22 PM +08:00

--- L9/app1.py
+++ L9/app1.py
@@ -8,4 +8,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_template("greet.html", name=request.args.get("name", "world"))
