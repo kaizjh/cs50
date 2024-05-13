aee5a9b0c890eafc406b85ccf52cbe47e663678d
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-13 07:17:45 +0000

    Mon, May 13, 2024, 3:17 PM +08:00

--- L9/app.py
+++ L9/app.py
@@ -9,3 +9,8 @@ app = Flask(__name__)
 
 def index():
     return render_template("index.html")
+
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
