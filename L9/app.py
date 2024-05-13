2e04cf71439402720c449f9f9eb462067f1ba82d
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-13 01:29:25 +0000

    Mon, May 13, 2024, 9:29 AM +08:00

--- L9/app.py
+++ L9/app.py
@@ -3,3 +3,6 @@ from flask import Flask, render_template, request
 # Underscore underscore name underscore underscore refers to the current file,
 # and so this is just a little trick that says, turn this file into a Flask application
 app = Flask(__name__)
+
+# This is known as a decorator in Python, not a flask feature
+@app.route()
