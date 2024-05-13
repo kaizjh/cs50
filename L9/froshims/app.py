a780421d69ced8f3dc15b4b3489ea92f01b91532
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-13 14:05:09 +0000

    Mon, May 13, 2024, 10:05 PM +08:00

--- L9/froshims/app.py
+++ L9/froshims/app.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+registants = {}
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/register", methods=["POST"])
 def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    registrants[name] = sport
     return render_template("success.html")
+
+@app.route("/registrants")
+def registrants():
+    return 
