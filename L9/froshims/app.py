cf89fad5303e7948e6880503011fa4af8b43d20a
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-14 01:30:49 +0000

    Tue, May 14, 2024, 9:30 AM +08:00

--- L9/froshims/app.py
+++ L9/froshims/app.py
@@ -19,13 +19,13 @@ def register():
     name = request.form.get("name")
     if not name:
         return render_template("failure.html")
+
     sport = request.form.get("sport")
     if sport not in SPORTS:
         return render_template("failure.html")
-    else:
-        REGISTRANTS[name] = sport
-        return render_template("success.html")
 
+    REGISTRANTS[name] = sport
+    return render_template("success.html")
 
 
 @app.route("/registrants")
