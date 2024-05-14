89320eb076481f12a32f97aef153206dd58afd4d
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-14 02:19:55 +0000

    Tue, May 14, 2024, 10:19 AM +08:00

--- L9/froshims4/app.py
+++ L9/froshims4/app.py
@@ -1,9 +1,11 @@
+# Implements a registration form, storing registrants in a SQL database
 
+from cs50 import SQL
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-REGISTRANTS = {}
+db = SQL("sqlite:///froshims.db")
 
 SPORTS = [
     "Basketball",
@@ -11,23 +13,36 @@ SPORTS = [
     "Ultimate Frisbee"
 ]
 
+
 @app.route("/")
 def index():
     return render_template("index.html", sports=SPORTS)
 
+
+@app.route("/deregister", methods=["POST"])
+def deregister():
+
+    # Forget registrant
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM registrants WHERE id =?", id)
+    return redirect("/registrants")
+
+
 @app.route("/register", methods=["POST"])
 def register():
+
+    # Validate submission
     name = request.form.get("name")
-    if not name:
-        return render_template("failure.html")
-
     sport = request.form.get("sport")
-    if sport not in SPORTS:
+    if not name or sport not in SPORTS:
         return render_template("failure.html")
 
-    REGISTRANTS[name] = sport
-    return render_template("success.html")
+    # Remember registrant
+    db.execute("INSERT INTO registrants (name, sport) VALUE(?,?)", name, sport)
 
+    # Confirm registration
+    return redirect("/registrants")
 
 @app.route("/registrants")
 def registrants():
