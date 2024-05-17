0d240147f4716a5ecd39871ee0ee0044c4830c50
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-17 15:19:23 +0000

    Fri, May 17, 2024, 11:19 PM +08:00

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -83,11 +83,11 @@ def buy():
             else:
                 # Record the transaction
                 former = usd(cash)
-                cash = usd(cash - total)
+                cash = cash - total
                 time = datetime.datetime.now()
                 price = usd(stock["price"])
                 db.execute("INSERT INTO buy(user_id, symbol, price, shares, cash, time) VALUES(?, ?, ?, ?, ?, ?)", user_id, symbol, price, shares, cash, time)
-                return render_template("bought.html", symbol=symbol, shares=shares, price=price, cash=cash, total=usd(total), former=former)
+                return render_template("bought.html", symbol=symbol, shares=shares, price=price, cash=usd(cash), total=usd(total), former=former)
 
 @app.route("/history")
 @login_required
