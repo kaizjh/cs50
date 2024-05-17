43922e9382939b577e2988640c316d941689ad6c
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-17 11:55:10 +0000

    Fri, May 17, 2024, 7:55 PM +08:00

--- ps9/finance/templates/temp.c
+++ ps9/finance/templates/temp.c
@@ -2,6 +2,6 @@ CREATE TABLE buy (
     user_id INTEGER NOT NULL,
     symbol TEXT NOT NULL,
     price NUMERIC NOT NULL,
-    cash NUMERIC NOT NULL,
+    cash NUMERIC NOT NULL DEFAULT 10000,
     FOREIGN KEY (user_id) REFERENCES users(id)
     );
