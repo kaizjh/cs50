7948fc535b496a72924e94de0f012f54df7b5a71
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-19 09:04:42 +0000

    Sun, May 19, 2024, 5:04 PM +08:00

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -115,7 +115,7 @@ def buy():
             else:
                 # Record the transaction
                 cash = cash - float(shares) * price
-                cash_formatted = round(cash, 2)
+                cash_formatted = f"{cash:.2f}"
                 time = datetime.datetime.now()
                 db.execute("INSERT INTO buy(user_id, symbol, price, shares, cash, time) VALUES(?, ?, ?, ?, ?, ?)", user_id, symbol, price, shares, cash_formatted, time)
 
@@ -289,7 +289,7 @@ def sell():
         price = owned["price"]
         cashs = db.execute("SELECT cash FROM buy WHERE user_id = ? ORDER BY time DESC LIMIT 1", user_id)[0]["cash"]
         cash = float(cashs) + float(shares) * float(price)
-        cash_formatted = round(cash, 2)
+        cash_formatted = f"{cash:.2f}"
         time = datetime.datetime.now()
         db.execute("INSERT INTO buy(user_id, symbol, price, shares, cash, time) VALUES(?, ?, ?, ?, ?, ?)", user_id, symbol, price, -int(shares), cash_formatted, time)
 
