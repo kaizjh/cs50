326db8eb12937ebd090e41186f1b0a3c74bb42ce
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-22 03:45:59 +0000

    Wed, May 22, 2024, 11:45 AM +08:00

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -71,9 +71,7 @@ def index():
         total = cash
         for stock in stocks:
             total = total + stock["value"]
-
-        print(usd(cash))
-        print()
+            
         return render_template("index.html", cash_usd=usd(cash), stocks=stocks, total_usd=usd(total), username=username, message=message)
 
 
