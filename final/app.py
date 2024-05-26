605694c86e6ddd0fc168d99e9c914f5f6292bd64
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-26 08:07:28 +0000

    Sun, May 26, 2024, 4:07 PM +08:00

--- final/app.py
+++ final/app.py
@@ -4,10 +4,16 @@ from flask_session import Session
 # Configure application
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
-app.configure["TEMPLATES_AUTO_RELOAD"] = True
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+# Disable caching of responses, lest you make a change to some file but your browser not notice. --Irving
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
