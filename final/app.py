24451df68ef5bcff3718e6a586dee55ba8c56cdf
Irvingzjh <146422785+Irvingzjh@users.noreply.github.com>
2024-05-26 08:06:28 +0000

    Sun, May 26, 2024, 4:06 PM +08:00

--- final/app.py
+++ final/app.py
@@ -1,4 +1,13 @@
+form flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+
 # Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
+app.configure["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
